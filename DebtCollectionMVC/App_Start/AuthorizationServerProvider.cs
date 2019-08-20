9f85ec47fc825c471d9cf81a21f216a6360b5f64
Muhamad Hilal <hilalramadan94@gmail.com>
2019-08-20 17:58:55 +0700

    Create AuthorizationServerProvider.cs
    Recovery lost files

---
+++ DebtCollectionMVC/App_Start/AuthorizationServerProvider.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using DebtCollectionMVC.Controllers;
+using DebtCollectionMVC.Models;
+
+namespace DebtCollectionMVC.App_Start
+{
+    public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            //var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            //context.Validated(identity);
+
+            //context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+           
+            using (AccountController _repo = new AccountController())
+            {
+                ApplicationUser user = await _repo.FindUser(context.UserName, context.Password);
+
+                if (user == null)
+                {
+                    context.SetError("invalid_grant", "The user name or password is incorrect.");
+                    return;
+                }
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+        }
+    }
+}
