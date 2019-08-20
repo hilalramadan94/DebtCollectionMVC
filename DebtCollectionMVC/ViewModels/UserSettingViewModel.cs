51c6075d82dff8355e2cd431476f1582de3846f8
Muhamad Hilal <hilalramadan94@gmail.com>
2019-08-20 18:17:03 +0700

    Create Files viewModels
    Recovery Lost Files

---
+++ DebtCollectionMVC/ViewModels/UserSettingViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DebtCollectionMVC.Models;
+
+namespace DebtCollectionMVC.ViewModels
+{
+    public class UserSettingViewModel
+    {
+        public ApplicationUser User { get; set; }
+    }
+}
---
+++ DebtCollectionMVC/ViewModels/VisitingResultViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DebtCollectionMVC.ViewModels
+{
+    public class VisitingResultViewModel
+    {
+        public int Id { get; set; }
+        public bool IsSuccess { get; set; }
+        public bool IsDebtorFound { get; set; }
+        public string Reason { get; set; }
+        public decimal? AmountPaid { get; set; }
+        public DateTime PromisedDate { get; set; }
+        public decimal GeoLat { get; set; }
+        public decimal GeoLng { get; set; }
+    }
+}
